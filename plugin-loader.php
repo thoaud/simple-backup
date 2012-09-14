7684c5aa77617b0d2d62875ef823d464941d2d40
ChrisHurst <ChrisHurst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-14 12:44:53 +0000

    tagging v1.1
    git-svn-id: https://plugins.svn.wordpress.org/simple-backup/tags/1.1@598970 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-loader.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Plugin Name: Simple Backup
+Plugin URI: http://MyWebsiteAdvisor.com/tools/wordpress-plugins/simple-backup/
+Description: Simple Backup System, You can create and download backups for your WordPress Website
+Version: 1.1
+Author: MyWebsiteAdvisor
+Author URI: http://MyWebsiteAdvisor.com
+*/
+
+register_activation_hook(__FILE__, 'simple_backup_activate');
+
+// display error message to users
+if ($_GET['action'] == 'error_scrape') {                                                                                                   
+    die("Sorry, Simple Backup Plugin requires PHP 5.0 or higher. Please deactivate Simple Backup Plugin.");                                 
+}
+
+function simple_backup_activate() {
+	if ( version_compare( phpversion(), '5.0', '<' ) ) {
+		trigger_error('', E_USER_ERROR);
+	}
+}
+
+// require simple backup Plugin if PHP 5 installed
+if ( version_compare( phpversion(), '5.0', '>=') ) {
+	define('SB_LOADER', __FILE__);
+
+	require_once(dirname(__FILE__) . '/simple-backup.php');
+	require_once(dirname(__FILE__) . '/plugin-admin.php');
+	
+	$simple_backup = new Simple_Backup_Admin();
+
+}
+?>
