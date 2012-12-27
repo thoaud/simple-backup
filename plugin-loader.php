9e1a83ecb44adb49d2ba4572cee5a70e4bba1dd5
ChrisHurst <ChrisHurst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-27 18:49:35 +0000

    tagging v2.5
    git-svn-id: https://plugins.svn.wordpress.org/simple-backup/tags/2.5@645093 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-loader.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: Simple Backup
+Plugin URI: http://MyWebsiteAdvisor.com/tools/wordpress-plugins/simple-backup/
+Description: Simple Backup System, You can create and download backups for your WordPress Website
+Version: 2.5
+Author: MyWebsiteAdvisor
+Author URI: http://MyWebsiteAdvisor.com
+*/
+
+register_activation_hook(__FILE__, 'simple_backup_activate');
+
+function simple_backup_activate() {
+
+	// display error message to users
+	if ($_GET['action'] == 'error_scrape') {                                                                                                   
+	    die("Sorry, Simple Backup Plugin requires PHP 5.3 or higher. Please deactivate Simple Backup Plugin.");                                 
+	}
+	
+	if ( version_compare( phpversion(), '5.3', '<' ) ) {
+		trigger_error('', E_USER_ERROR);
+	}
+	
+}
+
+
+// require simple backup Plugin if PHP 5 installed
+if ( version_compare( phpversion(), '5.3', '>=') ) {
+
+	define('SB_LOADER', __FILE__);
+
+	require_once(dirname(__FILE__) . '/simple-backup-manager.class.php');
+	require_once(dirname(__FILE__) . '/backup-list-table.class.php');
+	require_once(dirname(__FILE__) . '/simple-backup.php');
+	require_once(dirname(__FILE__) . '/plugin-admin.php');
+	
+	$simple_backup = new Simple_Backup_Admin();
+
+}
+
+?>
